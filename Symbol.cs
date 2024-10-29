9ef64bd0165dbb9e9fec6c79afa3a33b9d0b3a03
ArefOleg <serd-075@mail.ru>
2024-10-29 15:55:18 +0300

    update

--- Symbol.cs
+++ Symbol.cs
@@ -1,5 +1,9 @@
 namespace Symbol{
     public class Symbol{
-        
+        public string textSymbols;
+        public string path = @"symbols\symbols.txt";
+        public Symbol(){
+
+        }
     }
 }
