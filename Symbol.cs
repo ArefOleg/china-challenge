db0436d9c0de746b41db449d8d1bcabbdc74813f
ArefOleg <serd-075@mail.ru>
2024-10-29 16:47:54 +0300

    update

--- Symbol.cs
+++ Symbol.cs
@@ -1,9 +1,24 @@
-namespace Symbol{
-    public class Symbol{
-        public string textSymbols;
-        public string path = @"symbols\symbols.txt";
-        public Symbol(){
 
+namespace Symbol
+{
+    public class Symbol{
+        public string path = @"symbols/symbols.txt";
+        public List<string> arrayUndoubledTextSymbols;
+        public Symbol(){
+            readFileAsync();            
+        }
+
+        public async Task readFileAsync(){
+            using (StreamReader reader = new StreamReader(this.path)){
+                string textSymbols = await reader.ReadToEndAsync();
+                char[] arrayTextSymbols = textSymbols.ToCharArray();
+                this.arrayUndoubledTextSymbols = new List<string>();
+                foreach(var symbol in arrayTextSymbols){
+                    if(!arrayUndoubledTextSymbols.Contains(Convert.ToString(symbol))){
+                        Console.WriteLine(textSymbols.Length);
+                    }
+                }
+            }
         }
     }
 }
